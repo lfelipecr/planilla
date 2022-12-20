b7437915f38914571120ad2d6aaabb9dc4a466ed
FrancisVies <119692712+FrancisVies@users.noreply.github.com>
2022-12-20 01:00:23 -0600

    Add files via upload

--- model/hr_payslip.py
+++ model/hr_payslip.py
@@ -26,6 +26,11 @@ class hr_payslip_inherit_planilla(models.Model):
         rutas = self.env['rutas'].search([], limit=1)
         self.costo_blargos = rutas.barco_largo * self.cant_blargos
 
+    @api.onchange("cant_otros")
+    def change_otros(self):
+        rutas = self.env['rutas'].search([], limit=1)
+        self.costo_otros = rutas.otras_rutas * self.cant_otros
+
     @api.onchange("cant_locos")
     def change_locos(self):
         viaticos = self.env['viaticos'].search([], limit=1)
@@ -41,6 +46,20 @@ class hr_payslip_inherit_planilla(models.Model):
         viaticos = self.env['viaticos'].search([], limit=1)
         self.costo_feriados = viaticos.feriados * self.cant_feriados
 
+    def get_neto(self):
+        for rec in self:
+            rec.total_neto = 0
+            for line in rec.line_ids:
+                if line.code == "NET":
+                    rec.total_neto = line.total
+
+    def get_viaticos(self):
+        for rec in self:
+            rec.total_viaticos = 0
+            for line in rec.line_ids:
+                if line.code == "VIAT":
+                    rec.total_viaticos = line.total
+
     def get_total(self):
         for rec in self:
             rec.total_pagar = 0
@@ -53,6 +72,7 @@ class hr_payslip_inherit_planilla(models.Model):
         largos = 0
         bcortos = 0
         blargos = 0
+        otras = 0
         boletas = self.env['boleta'].search([('date', '>=', self.date_from), ('date', '<=', self.date_to), ('chofer', '=', self.employee_id.id)])
 
         for boleta in boletas:
@@ -64,6 +84,8 @@ class hr_payslip_inherit_planilla(models.Model):
                 bcortos = bcortos + 1
             if boleta.ruta2.tipo_ruta == "blargo":
                 blargos = blargos + 1
+            if boleta.ruta2.tipo_ruta == "otras":
+                otras = otras + 1
 
         self.cant_cortos = cortos
         self.change_cortos()
@@ -73,6 +95,8 @@ class hr_payslip_inherit_planilla(models.Model):
         self.change_bcortos()
         self.cant_blargos = blargos
         self.change_blargos()
+        self.cant_otros = otras
+        self.change_otros()
 
     codigo = fields.Integer(related="employee_id.codigo", string="Código Empleado")
     semana_pagar = fields.Integer("Semana a Pagar")
@@ -88,6 +112,8 @@ class hr_payslip_inherit_planilla(models.Model):
     costo_bcortos = fields.Float("Costo Barco Cortos")
     cant_blargos = fields.Integer("Cant. Barco Largos")
     costo_blargos = fields.Float("Costo Barco Largos")
+    cant_otros = fields.Integer("Cant. Otros")
+    costo_otros = fields.Float("Costo Otros")
 
     cant_locos = fields.Integer("Cant. Locos")
     costo_locos = fields.Float("Costo Locos")
@@ -108,4 +134,6 @@ class hr_payslip_inherit_planilla(models.Model):
     ahorro = fields.Float("Ahorro")
     otras_deduc = fields.Float("Otras Deduc.")
 
+    total_neto = fields.Float("Neto", compute="get_neto")
+    total_viaticos = fields.Float("Viáticos", compute="get_viaticos")
     total_pagar = fields.Float("Total", compute="get_total")
